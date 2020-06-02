dd328cf0ab067ee7fa357a6eb6dc7e4c6403f9e9
TJK1117 <tyler.kelso@daugherty.com>
2020-06-02 15:47:26 -0500

    Made some changes to reflect the stl regional funding map branding and added breaks in the content to space it out a bit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,16 +7,18 @@ import './Display.css'
 const Footer = (props) => {
 
   return (
-    <div className='support-wrapper'>
-      {/* <div className="support-title"><h4>Supported By:</h4></div> */}
-      <p>
-        Data were collected, cleaned, and analyzed by the <a target="_blank" href="http://stldata.org">St. Louis Regional Data Alliance</a>, with Liz Deichmann, the RDA’s Data and Research Manager, leading these efforts throughout 2019.
-        Data visualizations were created in partnership with <a target="_blank" href="http://daugherty.com">Daugherty Business Solutions</a>, which volunteered consultant time to construct the Tableau dashboard and user interface. <a target="_blank" href="http://helpingpeople.org">United Way of Greater St. Louis</a> was a pivotal supporter and thought partner throughout this process
-      </p>
-      <img src={rdaLogo} width="220" height='130' style={{ marginLeft: '20px' }} alt='Regional Data Alliance Logo' />
-      <img src={daughertyLogo} width="200" height='130' style={{ marginLeft: '20px' }} alt='Daugherty Business Solutions Logo' />
-      <img src={unitedWayLogo} width="200" height='140' style={{ marginLeft: '20px' }} alt="United Way of Saint Louis Logo" />
+    <div className="footer">
+      <div className='support-wrapper'>
+        <p>
+          Data were collected, cleaned, and analyzed by the <a target="_blank" rel="noopener noreferrer" href="http://stldata.org">St. Louis Regional Data Alliance</a>, with Liz Deichmann, the RDA’s Data and Research Manager, leading these efforts throughout 2019.
+        Data visualizations were created in partnership with <a target="_blank" rel="noopener noreferrer" href="http://daugherty.com">Daugherty Business Solutions</a>, which volunteered consultant time to construct the Tableau dashboard and user interface. <a target="_blank" rel="noopener noreferrer" href="http://helpingpeople.org">United Way of Greater St. Louis</a> was a pivotal supporter and thought partner throughout this process
+        </p>
+        <img src={rdaLogo} width="220" height='120' style={{ marginLeft: '20px' }} alt='Regional Data Alliance Logo' />
+        <img src={daughertyLogo} width="200" height='130' style={{ marginLeft: '20px' }} alt='Daugherty Business Solutions Logo' />
+        <img src={unitedWayLogo} width="200" height='140' style={{ marginLeft: '20px' }} alt="United Way of Saint Louis Logo" />
+      </div>
     </div>
+
   )
 }
 
--- src/Components/UnitedWayPage.jsx
+++ src/Components/UnitedWayPage.jsx
@@ -5,6 +5,7 @@ import Footer from './Footer'
 import { dashboardNames, urls } from '../Constants';
 import '../App.css'
 import { Select, MenuItem } from '@material-ui/core'
+import STLRegionalFundingMapLogo from '../STLRegionalFundingMapLogo.png'
 
 class UnitedWayPage extends Component {
   constructor(props) {
@@ -22,15 +23,21 @@ class UnitedWayPage extends Component {
     return (
       <div className="header">
         <NavBar />
+        <div className="spacer"></div>
+        <div className="funding-logo">
+          <img src={STLRegionalFundingMapLogo} alt="STL Regional Funding Map Logo" style={{ height: '150px', width: 'auto', marginTop: '50px' , marginBottom: '50px'}} />
+        </div>
+        <div className="spacer"></div>
         <div className='about-wrapper'>
           <h4 className='about-title'>About this Project</h4>
           <p className='about-text'>
-            The St. Louis Regional Funding Map tracks public and philanthropic funding awards over $50,000 in 2017, capturing almost $1.5 billion dollars and 2,735 distinct awards across hundreds of funders and recipients. 
-            Below, you can navigate a number of visualizations that break these awards down by geography, funder, and recipient — as well as explore and download the source data to conduct your own analysis. 
-            These data were collected and cleaned by the St. Louis Regional Data Alliance as part of the <a target="_blank" href="https://helpingpeople.org/communityneeds/">United Way’s Community Needs Assessment</a> released in January 2020. 
+            The St. Louis Regional Funding Map tracks public and philanthropic funding awards over $50,000 in 2017, capturing almost $1.5 billion dollars and 2,733 distinct awards across hundreds of funders and recipients.
+            Below, you can navigate a number of visualizations that break these awards down by geography, funder, and recipient — as well as explore and download the source data to conduct your own analysis.
+            These data were collected and cleaned by the St. Louis Regional Data Alliance as part of the <a target="_blank" rel="noopener noreferrer" href="https://helpingpeople.org/communityneeds/">United Way’s Community Needs Assessment</a> released in January 2020.
             Efforts are currently underway to expand these data to add additional years and more granular funding detail.
           </p>
         </div>
+        <div className="spacer"><hr /></div>
         <div className="select-wrapper">
           <h4 className='select-header'>Navigate to different boards here: </h4>
           <Select
@@ -44,7 +51,7 @@ class UnitedWayPage extends Component {
         </div>
         <DisplayManager display={this.state.chosenDisplay} url={urls[this.state.chosenDisplay]} />
         <Footer />
-        <div className="spacer"></div>
+        
       </div>
     );
   }
